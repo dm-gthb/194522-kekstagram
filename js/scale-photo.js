30d14c91eb1b289d693a94dc51f3c369f976389f
Dmitry Mamonov <dmitry.mamonov@dataart.com>
2018-06-23 20:56:30 +0300

    fixes

--- js/scale-photo.js
+++ js/scale-photo.js
@@ -6,31 +6,28 @@
   var SCALE_STEP_VALUE = 25;
   var processedImageContainerElement = document.querySelector('.img-upload__preview');
   var scalePlusElement = document.querySelector('.resize__control--plus');
+  var scaleControlElements = document.querySelectorAll('button.resize__control');
   var scaleMinusElement = document.querySelector('.resize__control--minus');
   var scaleValueElement = document.querySelector('.resize__control--value');
   var scaleValueNum = parseInt(scaleValueElement.value, 10);
 
-  function scalePlusElementClickHandler() {
-    if (scaleValueNum < SCALE_MAX_VALUE) {
-      scaleValueNum += SCALE_STEP_VALUE;
-      scaleMinusElement.disabled = false;
+  function scaleElementClickHandler(evt) {
+    var flag = evt.target.classList.contains('resize__control--plus') ? 1 : -1;
+    var newValue = scaleValueNum + SCALE_STEP_VALUE * flag;
+    if (newValue <= SCALE_MAX_VALUE && newValue >= SCALE_MIN_VALUE) {
+      if (newValue > scaleValueNum) {
+        scaleMinusElement.disabled = false;
+      } else {
+        scalePlusElement.disabled = false;
+      }
+      scaleValueNum = newValue;
+      scaleValueElement.value = scaleValueNum + '%';
+      addScaleStyle();
       if (scaleValueNum === SCALE_MAX_VALUE) {
         scalePlusElement.setAttribute('disabled', true);
-      }
-      scaleValueElement.value = scaleValueNum + '%';
-      addScaleStyle();
-    }
-  }
-
-  function scaleMinusElementClickHandler() {
-    if (scaleValueNum > SCALE_MIN_VALUE) {
-      scaleValueNum -= SCALE_STEP_VALUE;
-      scaleValueElement.value = scaleValueNum + '%';
-      scalePlusElement.disabled = false;
-      if (scaleValueNum === SCALE_MIN_VALUE) {
+      } else if (scaleValueNum === SCALE_MIN_VALUE) {
         scaleMinusElement.setAttribute('disabled', true);
       }
-      addScaleStyle();
     }
   }
 
@@ -39,11 +36,9 @@
     processedImageContainerElement.style.transform = 'scale' + '(' + scaleStyleValue + ')';
   }
 
-  scalePlusElement.addEventListener('click', function () {
-    scalePlusElementClickHandler();
-  });
-
-  scaleMinusElement.addEventListener('click', function () {
-    scaleMinusElementClickHandler();
-  });
+  for (var i = 0; i < scaleControlElements.length; i++) {
+    scaleControlElements[i].addEventListener('click', function (evt) {
+      scaleElementClickHandler(evt);
+    });
+  }
 })();
